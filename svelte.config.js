1de50f20df6ee37586d85b6be7ebaedb9fd789cc
Pelps <oluwapelps@gmail.com>
2022-04-16 20:30:07 -0500

    Adapt to Coudflare

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import cloudflare from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: cloudflare(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: cloudflare()
 	}
 };
 
