e5bde0048ab80ecc5e9dc9d9a4dc99a92beab626
Pelps <oluwapelps@gmail.com>
2022-08-26 20:18:17 -0500

    Change supabase to prisma

--- src/routes/api/register/+server.ts
+++ src/routes/api/register/+server.ts
@@ -1,4 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client/edge';
+
+const prisma = new PrismaClient();
 
 export const POST: RequestHandler = async ({ request }) => {
 	const ans: any = await request.formData();
@@ -11,15 +14,8 @@ export const POST: RequestHandler = async ({ request }) => {
 	}
 
 	try {
-		const res = await fetch(`${supabaseUrl}/rest/v1/people`, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				Prefer: 'return=headers-only',
-				apikey: supabaseAnonKey,
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
+		const member = await prisma.people.create({
+			data: {
 				email: ans.get('email'),
 				first_name: ans.get('first_name'),
 				last_name: ans.get('last_name'),
@@ -27,8 +23,8 @@ export const POST: RequestHandler = async ({ request }) => {
 				phone_number: ans.get('phone'),
 				netID: ans.get('netID'),
 				major: ans.get('major'),
-				is_member: false
-			})
+				is_paid: false
+			}
 		});
 
 		/* ([
@@ -44,97 +40,65 @@ export const POST: RequestHandler = async ({ request }) => {
 			}
 		]); */
 
-		if (res.ok) {
+		if (member !== null) {
 			//Make more efficient
-			console.log(res.headers.get('Location'));
-			const id = res.headers.get('Location').split('.')[1];
 			console.log(ans?.get('dance'));
 			const array = [
 				...(ans?.get('dance') === 'true'
 					? [
-							fetch(`${supabaseUrl}/rest/v1/Dance%20Interest`, {
-								method: 'POST',
-								headers: {
-									Accept: 'application/json',
-									apikey: supabaseAnonKey,
-									'Content-Type': 'application/json'
-								},
-								body: JSON.stringify({
+							prisma.dance_Interest.create({
+								data: {
 									name: ans.get('first_name') + ' ' + ans.get('last_name'),
 									phone_number: ans.get('phone'),
 									netID: ans.get('netID'),
 									email: ans.get('email')
-								})
+								}
 							})
 					  ]
 					: []),
 				...(ans?.get('basketball') === 'true'
 					? [
-							fetch(`${supabaseUrl}/rest/v1/Basketball%20List`, {
-								method: 'POST',
-								headers: {
-									Accept: 'application/json',
-									apikey: supabaseAnonKey,
-									'Content-Type': 'application/json'
-								},
-								body: JSON.stringify({
+							prisma.basketball_List.create({
+								data: {
 									name: ans.get('first_name') + ' ' + ans.get('last_name'),
 									phone_number: ans.get('phone'),
 									netID: ans.get('netID'),
 									email: ans.get('email')
-								})
+								}
 							})
 					  ]
 					: []),
 				...(ans?.get('volleyball') === 'true'
 					? [
-							fetch(`${supabaseUrl}/rest/v1/Volleyball%20List`, {
-								method: 'POST',
-								headers: {
-									Accept: 'application/json',
-									apikey: supabaseAnonKey,
-									'Content-Type': 'application/json'
-								},
-								body: JSON.stringify({
+							prisma.volleyball_List.create({
+								data: {
 									name: ans.get('first_name') + ' ' + ans.get('last_name'),
 									phone_number: ans.get('phone'),
 									netID: ans.get('netID'),
 									email: ans.get('email')
-								})
+								}
 							})
 					  ]
 					: []),
 				...(ans?.get('soccer') === 'true'
 					? [
-							fetch(`${supabaseUrl}/rest/v1/Soccer%20List`, {
-								method: 'POST',
-								headers: {
-									Accept: 'application/json',
-									apikey: supabaseAnonKey,
-									'Content-Type': 'application/json'
-								},
-								body: JSON.stringify({
+							prisma.soccer_List.create({
+								data: {
 									name: ans.get('first_name') + ' ' + ans.get('last_name'),
 									phone_number: ans.get('phone'),
 									netID: ans.get('netID'),
 									email: ans.get('email')
-								})
+								}
 							})
 					  ]
 					: []),
 				...(ans?.get('attendance') !== null
 					? [
-							fetch(`${supabaseUrl}/rest/v1/events_people`, {
-								method: 'POST',
-								headers: {
-									Accept: 'application/json',
-									apikey: supabaseAnonKey,
-									'Content-Type': 'application/json'
-								},
-								body: JSON.stringify({
-									people_id: id,
+							prisma.events_people.create({
+								data: {
+									people_id: member.id,
 									event_id: ans?.get('attendance')
-								})
+								}
 							})
 					  ]
 					: [])
@@ -159,8 +123,7 @@ export const POST: RequestHandler = async ({ request }) => {
 				});
 			}
 		} else {
-			const result = await res.json();
-			return new Response(result.message, {
+			return new Response(member?.id, {
 				status: 400,
 				headers: {
 					'Content-Type': 'application/json'
