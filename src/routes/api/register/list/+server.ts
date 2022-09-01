c04f948f215e60d7980eff29cddd32b979e9654f
Pelps <oluwapelps@gmail.com>
2022-08-31 22:22:06 -0500

    Add unsubscription page

--- src/routes/api/register/list/+server.ts
+++ src/routes/api/register/list/+server.ts
@@ -44,6 +44,45 @@ export const POST: RequestHandler = async ({ request }) => {
 	}
 };
 
+export const DELETE: RequestHandler = async ({ request }) => {
+	const ans: any = await request.formData();
+
+	const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+	const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+	const email: string = DOMPurify.sanitize(ans.get('email'));
+	if (!validateEmail(email)) {
+		return new Response(JSON.stringify({ body: 'Not a valid email' }), {
+			status: 400,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+	console.log(ans);
+
+	const res = await fetch(`${supabaseUrl}/rest/v1/Mailing%20List?email=eq.${email}`, {
+		method: 'DELETE',
+		headers: {
+			Accept: 'application/json',
+			apikey: supabaseAnonKey,
+			'Content-Type': 'application/json'
+		}
+	});
+	if (res.ok) {
+		return new Response(null, {
+			status: 204
+		});
+	} else {
+		const result = await res.json();
+		return new Response(JSON.stringify({ body: result.message }), {
+			status: 400,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+};
+
 const validateEmail = (email: string) => {
 	return email.match(
 		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
---
+++ src/routes/unsubscribe/+page.svelte
@@ -0,0 +1,102 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	import { supabase } from '$lib/supabaseClient';
+	import { string } from 'yup';
+	import { personId } from '../../stores/personStore';
+	export let id;
+
+	let success = false;
+
+	const submitAttendance = async () => {
+		try {
+			const { data, error } = await supabase.from('Mailing List').delete().match({
+				email: values.email
+			});
+			if (error) {
+				console.log(error);
+				showMessage('email_message', error.message);
+			} else {
+				showMessage('email_message', 'Successfully Removed');
+				console.log(msgs.email_message);
+				setTimeout(() => {
+					goto('/');
+				}, 450);
+			}
+			//console.log(data);
+		} catch (error) {
+			showMessage('email_message', error.message);
+			if (error.message === 'You are not in the mailing list') {
+				setTimeout(() => {
+					goto(`/`);
+				}, 500);
+			}
+		}
+	};
+
+	let values = {};
+	let msgs = {};
+
+	const resetField = (name) => {
+		msgs[name] = null;
+		const error_element = document.getElementById(name);
+		error_element.style.visibility = 'hidden';
+	};
+
+	const showMessage = (id, message) => {
+		console.log(id);
+		const error_element = document.getElementById(id);
+		console.log(error_element);
+		msgs[id] = message;
+		setTimeout(() => {
+			error_element.style.visibility = 'visible';
+		}, 60);
+	};
+</script>
+
+<div class="flex justify-center items-center">
+	<div>
+		<form class="my-6 w-96" on:submit|preventDefault={submitAttendance}>
+			<div class="relative z-0 w-full mb-6 ">
+				<input
+					type="email"
+					color="green"
+					name="floating_email"
+					on:focus={() => resetField('email_message')}
+					id="email"
+					class={` text-xl block py-2.5 px-0 w-full   bg-transparent border-0 border-b-2 ${
+						msgs.email_message == null
+							? 'border-gray-300 text-neutral'
+							: msgs.email_message === 'Successfully Removed'
+							? 'border-success text-success'
+							: 'border-error text-error'
+					}  appearance-none dark:text-neutral dark:border-gray-600 dark:focus:border-neutral focus:outline-none focus:ring-0 focus:border-secondary peer `}
+					placeholder=" "
+					bind:value={values.email}
+				/>
+				<label
+					for="floating_email"
+					class={`peer-focus:font-medium absolute text-lg ${
+						msgs.email_message == null
+							? 'text-neutral'
+							: msgs.email_message === 'Successfully Removed'
+							? 'text-success'
+							: 'text-error'
+					} dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-secondary peer-focus:dark:text-neutral peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8`}
+					>Email</label
+				>
+				<p
+					class={`invisible ${
+						msgs.email_message === 'Successfully Removed' ? 'text-success' : 'text-error'
+					}`}
+					id="email_message"
+				>
+					{`${msgs.email_message}`}
+				</p>
+			</div>
+			<div class="">
+				<button type="submit" class="btn btn-primary">SUBMIT</button>
+			</div>
+		</form>
+	</div>
+</div>
