d810d2c97650abb9d10544c7841b696a660482ad
Pelps12 <oluwapelps@gmail.com>
2023-08-14 18:52:14 -0500

    Should work now

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import { DB_URL } from '$env/static/private';
 import pkg from 'pg';
+import postgres from 'postgres';
 const { Client } = pkg;
-export const pool = new Client({ connectionString: DB_URL });
-export const db = drizzle(pool);
+export const client = postgres(DB_URL);
+export const db = drizzle(client);
