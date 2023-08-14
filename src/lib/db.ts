501dfc2a6feb53a873cd7da9989d100647b5323c
Pelps12 <oluwapelps@gmail.com>
2023-08-14 04:50:09 -0500

    Make new changes :)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { DB_URL } from '$env/static/private';
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 export const pool = new Pool({ connectionString: DB_URL });
 export const db = drizzle(pool);
