7dd0fa4966c4c0d3508faf8223bd244539204687
Pelps12 <oluwapelps@gmail.com>
2023-08-14 14:15:10 -0500

    Please work

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { DB_URL } from '$env/static/private';
 import pkg from 'pg';
-const { Pool } = pkg;
-export const pool = new Pool({ connectionString: DB_URL });
+const { Client } = pkg;
+export const pool = new Client({ connectionString: DB_URL });
 export const db = drizzle(pool);
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,7 @@
 import { events } from '../../drizzle/schema';
 import { db, pool } from '../lib/db';
 import type { PageServerLoad } from './$types';
-import { wrap } from '$lib/safeAPICall';
-export const load = async ({ params, platform }) => {
+export const load: PageServerLoad = async ({ params, platform }) => {
 	const dbEvents = await db.select().from(events).orderBy(events.importance).limit(4);
 
 	return {
