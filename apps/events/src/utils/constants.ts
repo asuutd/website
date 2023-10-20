c3ad8035fabb31bb5795072da65216bc440a5aee
Jason Antwi-Appah <hey@jasonaa.me>
2023-10-19 23:54:55 -0500

    Fix file upload limit

--- apps/events/src/utils/constants.ts
+++ apps/events/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const MAX_FILE_SIZE = 500000;
+export const MAX_FILE_SIZE = 5_000_000; // 5mb in bytes
 export const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
 
 /**
