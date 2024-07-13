a6f7505da4e7e5220095ee8bad3299575178b071
Jason Antwi-Appah <hey@jasonaa.me>
2024-07-13 11:54:45 -0500

    feat: impl sync members endpoint

--- apps/family-system/src/payload.config.ts
+++ apps/family-system/src/payload.config.ts
@@ -9,7 +9,7 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Members } from './collections/Members'
 import { Families } from './collections/Families'
-import { getMembers } from './jonze'
+import { getMember, getMembers } from './jonze'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -35,11 +35,63 @@ export default buildConfig({
   ],
   endpoints: [
     {
-      path: '/api/sync-members',
+      path: '/sync-members',
       method: 'post',
-      handler: async () => {
-        const members = await getMembers()
-        return Response.json(members)
+      handler: async (req) => {
+        if (!req.user) {
+          return new Response('Unauthorized', {
+            status: 401,
+            headers: {
+              'Content-Type': 'text/plain',
+            },
+          })
+        }
+
+        const encoder = new TextEncoder();
+        const stream = new ReadableStream({
+            async start(controller) {
+                const sendLog = (data: string) => {
+                  const date = new Date();
+                  const out = `${date.toLocaleTimeString()}: ${data}\n` 
+                  const chunkData = encoder.encode(out);
+                  controller.enqueue(chunkData);
+                }
+
+                sendLog(`Getting members`)
+                const members = await getMembers()
+
+                sendLog(`Got ${members.length} members`)
+
+                for (const member of members) {
+                  sendLog(`Getting member ${member.id}`)
+                  const memberData = await getMember(member.id)
+                  sendLog(`Got member ${member.id}`)
+
+                  sendLog(`Creating member ${member.id}`)
+                  const body = {
+                    jonze_member_id: memberData.id,
+                    jonze_name: memberData.user.firstName + ' ' + memberData.user.lastName,
+                    jonze_tags: memberData.tags?.names ?? [],
+                  }
+                  await req.payload.db.drizzle.insert(req.payload.db.tables.members).values(body).onConflictDoUpdate({
+                    target: req.payload.db.tables.members.jonze_member_id,
+                    set: body})
+                  sendLog(`Created member ${member.id}`)
+                }
+
+                sendLog(`Done!`)
+                controller.close();
+            }
+        });
+
+        return new Response(stream, {
+          status: 200,
+          headers: {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache, no-transform',
+            'Connection': 'keep-alive',
+          },
+        })
       }
     }
   ]
