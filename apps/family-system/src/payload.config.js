7848279cc4163a5bd59122a714aa1a9ed7e78014
Jason Antwi-Appah <hey@jasonaa.me>
2024-07-13 14:14:50 -0500

    fix: handle errors in member sync endpoint

--- apps/family-system/src/payload.config.js
+++ apps/family-system/src/payload.config.js
@@ -36,7 +36,7 @@ export default buildConfig({
   ],
   endpoints: [
     {
-      path: '/sync-members',
+      path: '/sync-jonze-members',
       method: 'post',
       handler: async (req) => {
         if (!req.user) {
@@ -53,35 +53,39 @@ export default buildConfig({
             async start(controller) {
                 const sendLog = (data) => {
                   const date = new Date();
-                  const out = `${date.toLocaleTimeString()}: ${data}\n` 
+                  const out = `${date.toLocaleTimeString()}.${date.getMilliseconds()}: ${data}\n` 
                   const chunkData = encoder.encode(out);
                   controller.enqueue(chunkData);
                 }
 
-                sendLog(`Getting members`)
-                const members = await getMembers()
+                try {
+                  sendLog(`Getting member list`)
+                  const members = await getMembers()
 
-                sendLog(`Got ${members.length} members`)
+                  sendLog(`Got ${members.length} members`)
 
-                for (const member of members) {
-                  sendLog(`Getting member ${member.id}`)
-                  const memberData = await getMember(member.id)
-                  sendLog(`Got member ${member.id}`)
+                  for (const member of members) {
+                    sendLog(`Getting member ${member.id}`)
+                    const memberData = await getMember(member.id)
+                    sendLog(`Got member ${member.id}`)
 
-                  sendLog(`Creating member ${member.id}`)
-                  const body = {
-                    jonze_member_id: memberData.id,
-                    jonze_name: ((memberData.user.firstName ?? ' ') + ' ' + (memberData.user.lastName ?? '')).trim(),
-                    jonze_tags: memberData.tags?.names ?? [],
+                    sendLog(`Creating member ${member.id}`)
+                    const body = {
+                      jonze_member_id: memberData.id,
+                      jonze_name: ((memberData.user.firstName ?? ' ') + ' ' + (memberData.user.lastName ?? '')).trim(),
+                      jonze_tags: memberData.tags?.names ?? [],
+                    }
+                    await req.payload.db.drizzle.insert(req.payload.db.tables.members).values(body).onConflictDoUpdate({
+                      target: req.payload.db.tables.members.jonze_member_id,
+                      set: body})
+                    sendLog(`Created member ${member.id}`)
                   }
-                  await req.payload.db.drizzle.insert(req.payload.db.tables.members).values(body).onConflictDoUpdate({
-                    target: req.payload.db.tables.members.jonze_member_id,
-                    set: body})
-                  sendLog(`Created member ${member.id}`)
+                  sendLog(`Done!`)
+                } catch (e) {
+                  sendLog(`An error occurred:\n${JSON.stringify(e)}`)
+                } finally {
+                  controller.close();
                 }
-
-                sendLog(`Done!`)
-                controller.close();
             }
         });
 
