cc8715e1558a598abcfd0ec5f0295ccf3628fa4b
Jason Antwi-Appah <hey@jasonaa.me>
2024-07-13 14:54:49 -0500

    feat: update family score on ledger update

--- apps/family-system/src/collections/LedgerEntries.ts
+++ apps/family-system/src/collections/LedgerEntries.ts
@@ -1,10 +1,46 @@
-import type { CollectionConfig } from 'payload'
+import { LedgerEntry } from '@/payload-types'
+import type { CollectionConfig, CollectionAfterChangeHook, CollectionAfterDeleteHook } from 'payload'
+import { sql, eq } from 'drizzle-orm'
+
+const UpdateFamilyScore: CollectionAfterChangeHook<LedgerEntry> = async ({doc, req, previousDoc}) => {
+    if (previousDoc) {
+        // previousDoc is null for new documents. We only want to run this hook on new documents.
+        // TODO: handle fully recalculating the family score when updating a ledger entry
+        return
+    }
+    const familyId = typeof doc.Family === 'number' ? doc.Family : doc.Family.id
+
+    const table = req.payload.db.tables.families
+    await req.payload.db.drizzle.update(table).set({
+            score: sql`${table.score} + ${doc.amount}`,
+    // @ts-ignore
+    }).where(eq(table.id, familyId))
+
+}
+
+const UpdateFamilyScoreOnDelete: CollectionAfterDeleteHook<LedgerEntry> = async ({doc, req}) => {
+    const familyId = typeof doc.Family === 'number' ? doc.Family : doc.Family.id
+
+    const table = req.payload.db.tables.families
+    await req.payload.db.drizzle.update(table).set({
+            score: sql`${table.score} - ${doc.amount}`,
+    // @ts-ignore
+    }).where(eq(table.id, familyId))
+}
 
 export const LedgerEntries: CollectionConfig = {
     slug: 'ledger_entries',
     admin: {
         useAsTitle: 'description',
         description: 'Ledger entries for the family system. Use this to award or deduct points from families.'
+    },
+    hooks: {
+        afterChange: [
+            UpdateFamilyScore,
+        ],
+        afterDelete: [
+            UpdateFamilyScoreOnDelete
+        ],
     },
     labels: {
         singular: 'Ledger Entry',
