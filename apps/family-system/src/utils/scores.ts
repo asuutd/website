1a5748f8b9b6acac4df8579bb0ead53566d269f8
Jason Antwi-Appah <hey@jasonaa.me>
2024-07-15 00:01:23 -0500

    fix: attempt to use cte for `recalculateScores`

--- apps/family-system/src/utils/scores.ts
+++ apps/family-system/src/utils/scores.ts
@@ -4,24 +4,21 @@ import type { BasePayload } from 'payload'
 // TODO: use prepared statements - https://orm.drizzle.team/docs/perf-queries#prepared-statement
 
 export const recalculateScores = async (payload: BasePayload) => {
-    const ledger_entries = payload.db.tables.ledger_entries
-    const newScores = await payload.db.drizzle.select({
-        familyId: ledger_entries.Family, 
-        newScore: sum(ledger_entries.amount).as('new_score')
-    }).from(ledger_entries)
-    .groupBy(ledger_entries.Family)
+    const {ledger_entries, families} = payload.db.tables
+    const {drizzle: db} = payload.db
 
-    for (const { newScore, familyId } of newScores) {
-        await payload.update(
-            {
-                collection: 'families',
-                id: familyId as number,
-                data: {
-                    score: Number(newScore) ?? 0
-                }
-            }
-        )
-    }
+    const newScores = db.$with('newScores').as(
+        db.select({
+            familyId: ledger_entries.Family, 
+            score: sum(ledger_entries.amount).as('score')
+        }).from(ledger_entries)
+        .groupBy(ledger_entries.Family)
+    )
+
+    // TODO: figure out why this doesn't work... the docs claim that this should work, but it doesn't
+    // const update = await db.with(newScores).update(families).set({
+    //     score: newScores.score
+    // }).where(eq(families.id, newScores.familyId))
 }
 
 export const getTopMemberPointEarners = async (payload: BasePayload, limit = 5) => {
