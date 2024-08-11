a5d3cf65e1522bf868135221690b05d30f481704
Jason Antwi-Appah <hey@jasonaa.me>
2024-08-10 19:28:12 -0500

    fix: import JonzeClient from new package

--- apps/family-system/src/utils/jonze.ts
+++ apps/family-system/src/utils/jonze.ts
@@ -1,6 +1,6 @@
 'use server';
 
 import { env } from '@/env/server.mjs';
-import { JonzeClient } from './JonzeClient';
+import { JonzeClient } from '@asu/jonze';
 
 export const jonzeClient = new JonzeClient(env.JONZE_API_KEY, !!env.USE_JONZE_DEV);
