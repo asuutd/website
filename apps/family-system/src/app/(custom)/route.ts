f54d60a0781aa4f339c2cba9f6329d3028cdf588
Jason Antwi-Appah <hey@jasonaa.me>
2024-08-28 15:00:09 -0500

    fix(family): use correct filename for route handler

--- apps/family-system/src/app/(custom)/route.ts
+++ apps/family-system/src/app/(custom)/route.ts
@@ -3,3 +3,7 @@ import { redirect } from 'next/navigation'
 export async function GET(request: Request) {
   redirect('/admin')
 }
+
+export async function HEAD(request: Request) {
+  redirect('/admin')
+}
